a059338a7fdb4b847f3a6cc160dae84527d13ec4
Victor <36003172+VictorGhosh@users.noreply.github.com>
2021-05-22 20:42:37 -0400

    Init

---
+++ display.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import time
+import argparse
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+def init(n, block_orientation, rotate, inreverse) :
+    # create matrix device
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
+    print("device init success")
+    return device
+
+def get_message() :
+    f = open("message.txt")
+    msg = f.read()
+    f.close()
+    return msg
+
+def slow_scroll(msg, n, block_orientation, rotate, inreverse) :
+    # create matrix device
+    device = init(n, block_orientation, rotate, inreverse)
+
+    print("printing message")
+    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
+    print("message complete")
+
+def led_print(msg) :
+    parser = argparse.ArgumentParser(description='matrix_demo arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+
+    args = parser.parse_args()
+
+    slow_scroll(msg, args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
+
+if __name__ == "__main__" :
+    try :
+        while True :
+            led_print(get_message())
+    except KeyboardInterrupt :
+        pass
